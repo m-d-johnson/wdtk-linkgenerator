7bcbbfb0762633c28da927fb8773ac6152adfbd4
Mike Johnson <mdj@mikejohnson.xyz>
2023-10-06 23:24:01 +0100

    Updates (#1)
    * general fucking around
    
    * Loads of updates
    
    * Loads of updates. Generate files directly rather
    than by redirecting stdout to a file.
    Some housekeeping.
    
    * Some housekeeping.

--- main.py
+++ main.py
@@ -1,95 +1,118 @@
+import csv
 import datetime
 import json
+import os
+import pathlib
 import sys
-import requests
 from datetime import datetime
-import csv
+
+import mdformat
+import requests
+from mysoc_dataset import get_dataset_url
+from opml import OpmlDocument
 
 # TODO: Non-Home Office forces: MDP, BTP, CNC, etc
 # TODO: Get Disclosure Log URLs: Need to talk to MyDemocracy
-# TODO: Reduce the replication in this script: It's a quick and dirty tool for something.
+# TODO: Reduce the replication in this script.
 
-from opml import OpmlDocument
 
-document = OpmlDocument(
-    title='United Kingdom Home Office (Territorial) Police Forces and Associated Bodies',
+document: OpmlDocument = OpmlDocument(
+    title='United Kingdom Police Forces and Associated Bodies',
     owner_name='Mike Johnson',
-    owner_email='mdj.uk@pm.me'
-)
+    owner_email='mdj.uk@pm.me')
+
+# Prepare a file to use for the generated output
+markdown_output_file = open('overview.md', 'wt')
 
 
-def print_header():
-    print("# Generated List of Police Forces (WikiData/WhatDoTheyKnow)")
-    print("*This list may not yet include certain forces*")
-    print("\n")
-    print("[OPML File Available](https://github.com/m-d-johnson/wdtk-linkgenerator/blob/master/police.opml)")
-    print("\n")
+def get_csv_dataset():
+    # get the url of dataset
+    url = get_dataset_url(repo_name="wdtk_authorities_list",
+                          package_name="whatdotheyknow_authorities_dataset",
+                          version_name="latest",
+                          file_name="authorities.csv", )
+    r = requests.get(url)
+    open('authorities.csv', 'wb').write(r.content)
 
-    print("|Organisation Name|Website|WDTK Org Page|WDTK JSON|Atom Feed|JSON Feed|Publication Scheme|")
-    print("|-|-|-|-|-|-|-|")
+
+def generate_header():
+    body = "# Generated List of Police Forces (WikiData/WhatDoTheyKnow) \n\n"
+    body += "*Depending on the sources from which it was generated, this list may not include certain forces* \n"
+    body += "*It is generated from data provided by WhatDoTheyKnow. If there are inaccuracies, please contact* \n"
+    body += "*them with corrections. The table below will then be corrected when the script is next run.* \n\n"
+    body += "[OPML File Available](https://github.com/m-d-johnson/wdtk-linkgenerator/blob/master/police.opml) \n\n"
+    body += "|Organisation Name|Website|WDTK Org Page|WDTK JSON|Atom Feed|JSON Feed|Publication Scheme| \n"
+    body += "|-|-|-|-|-|-|-| "
+    return body
 
 
 def process_one_csv_file(csv_file_name):
+    # Open CSV file and read it into the list of rows
     rows = []
-    results = []
-    # reading csv file
     with open(csv_file_name, 'r') as csvfile:
-        # creating a csv reader object
         csvreader = csv.reader(csvfile)
-        # extracting each data row one by one
         for row in csvreader:
             rows.append(row)
-    print_header()
+
+    markdown_output_file.write(generate_header() + "\n")
+
+    # Iterate over all the entries in the CSV import, and generate a row in a
+    # Markdown table
+    results = []
     for row in rows:
         name_of_org = row[0]
         wdtk = row[1]
         url = f"https://www.whatdotheyknow.com/body/{wdtk}"
-        pubscheme = url
-        response = requests.get(f"https://www.whatdotheyknow.com/body/{wdtk}.json")
+        response = requests.get(f"{url}.json")
         wdtk_data = response.json()
+
+        # If they've provided a link to a publication scheme, use that. Else,
+        # leave it set to the WDTK overview.
         if wdtk_data['publication_scheme']:
             pubscheme = wdtk_data['publication_scheme']
         else:
-            pubscheme = "https://whatdotheyknow.com/"
+            pubscheme = url  # Just redirect to overview page
+
+        # If they've provided a home page, use that. Else, leave it set to the
+        # WDTK overview.
         if wdtk_data['home_page']:
             url = wdtk_data['home_page']
-        else:
-            url = "https://whatdotheyknow.com/"
 
-        row = f"|{name_of_org} | "
-        row += f"[Website]({url})|"
-        row += f"[wdtk page](https://www.whatdotheyknow.com/body/{wdtk})|"
-        row += f"[wdtk json](https://www.whatdotheyknow.com/body/{wdtk}.json)|"
-        row += f"[wdtk atom feed](https://www.whatdotheyknow.com/feed/body/{wdtk})|"
-        row += f"[wdtk json feed](https://www.whatdotheyknow.com/feed/body/{wdtk}.json)|"
-        row += f"[Publication Scheme]({pubscheme})|"
-        results.append(row)
+        # Construct the row in the Markdown table
+        entry = ''
+        entry += f"|{name_of_org} | "
+        entry += f"[Website]({url})|"
+        entry += f"[wdtk page](https://www.whatdotheyknow.com/body/{wdtk})|"
+        entry += f"[wdtk json](https://www.whatdotheyknow.com/body/{wdtk}.json)|"
+        entry += f"[atom feed](https://www.whatdotheyknow.com/feed/body/{wdtk})|"
+        entry += f"[json feed](https://www.whatdotheyknow.com/feed/body/{wdtk}.json)|"
+        entry += f"[Publication Scheme]({pubscheme})|"
+        entry += "\n"
 
-        feed = document.add_rss(
-            f"{name_of_org} FOI Disclosures",
-            f"https://www.whatdotheyknow.com/feed/body/{wdtk}",
-            version='RSS2',
-            created=datetime.now()
-        )
+        # results[] is a list of these rows. Append to it.
+        results.append(entry)
 
-    document.dump('police.opml', pretty=True)
+        # Also add it to the list of RSS feeds
+        document.add_rss(f"{name_of_org} FOI Disclosures",
+                         f"https://www.whatdotheyknow.com/feed/body/{wdtk}",
+                         version='RSS2',
+                         created=datetime.now())
 
+    # For ease of reading, sort the rows alphabetically before
+    # we proceed to writing them to a file.
     results.sort()
+    # Then, write the rows to the file and close it. The header has already been
+    # written.
     for i in results:
-        print(i)
-
-
-# wdtk page: https://www.whatdotheyknow.com/body/{name_of_org}
-# wdtk atom feed: https://www.whatdotheyknow.com/feed/body/{name_of_org}
-# wdtk json feed: https://www.whatdotheyknow.com/feed/body/{name_of_org}.json
+        markdown_output_file.write(str(i))
 
 
 def process_one_json_file(json_file_name):
     fh = open(json_file_name, 'r')
     imported_json = json.load(fh)
+    markdown_output_file.write(generate_header() + "\n")
 
     results = []
-    print_header()
     for item in imported_json:
         name_of_org = str(item['itemLabel'])
         url = str(item['website'])
@@ -99,37 +122,43 @@ def process_one_json_file(json_file_name):
         if wdtk_data['publication_scheme']:
             pubscheme = wdtk_data['publication_scheme']
         else:
-            pubscheme = "https://whatdotheyknow.com/"
+            pubscheme = f"https://whatdotheyknow.com/body/{wdtk}"
 
-        row = f"|{name_of_org} | "
-        row += f"[Website]({url})|"
-        row += f"[wdtk page](https://www.whatdotheyknow.com/body/{wdtk})|"
-        row += f"[wdtk json](https://www.whatdotheyknow.com/body/{wdtk}.json)|"
-        row += f"[wdtk atom feed](https://www.whatdotheyknow.com/feed/body/{wdtk})|"
-        row += f"[wdtk json feed](https://www.whatdotheyknow.com/feed/body/{wdtk}.json)|"
-        row += f"[Publication Scheme]({pubscheme})|"
-        results.append(row)
+        entry = f"|{name_of_org} | "
+        entry += f"[Website]({url})|"
+        entry += f"[wdtk page](https://www.whatdotheyknow.com/body/{wdtk})|"
+        entry += f"[wdtk json](https://www.whatdotheyknow.com/body/{wdtk}.json)|"
+        entry += f"[wdtk atom feed](https://www.whatdotheyknow.com/feed/body/{wdtk})|"
+        entry += f"[wdtk json feed](https://www.whatdotheyknow.com/feed/body/{wdtk}.json)|"
+        entry += f"[Publication Scheme]({pubscheme})|"
+        entry += "\n"
 
-        feed = document.add_rss(
-            f"{name_of_org} FOI Disclosures",
-            f"https://www.whatdotheyknow.com/feed/body/{wdtk}",
-            version='RSS2',
-            created=datetime.now()
-        )
+        results.append(entry)
 
-    document.dump('police.opml', pretty=True)
+        document.add_rss(f"{name_of_org} FOI Disclosures",
+                         f"https://www.whatdotheyknow.com/feed/body/{wdtk}",
+                         version='RSS2',
+                         created=datetime.now())
 
     results.sort()
     for i in results:
-        print(i)
+        markdown_output_file.write(str(i))
 
 
-# wdtk page: https://www.whatdotheyknow.com/body/{name_of_org}
-# wdtk atom feed: https://www.whatdotheyknow.com/feed/body/{name_of_org}
-# wdtk json feed: https://www.whatdotheyknow.com/feed/body/{name_of_org}.json
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    for filename in sys.argv[1:]:
-        # process_one_json_file(filename)
+    filename = sys.argv[1]
+    if filename.endswith('.json'):
+        process_one_json_file(filename)
+    elif filename.endswith('.csv'):
+        get_csv_dataset()
         process_one_csv_file(filename)
+
+    markdown_output_file.close()
+    document.dump('police.opml', pretty=True)
+    filepath = pathlib.Path("overview.md")
+    mdformat.file(filepath)
+    if os.path.exists("authorities.csv"):
+        os.remove("authorities.csv")
+    else:
+        print("The WDTK CSV file does not exist, so could not be deleted.")
+    os.link('overview.md', 'out.md')
