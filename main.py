9573adcde93026099bad924a6a1df5d9aea1c544
Mike Johnson <mdj@mikejohnson.xyz>
2023-10-09 09:46:04 +0100

    Tidying up, fix args and modes, logic

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import argparse
 import csv
 import datetime
-import encodings
 import json
 import os
 import pathlib
-import pprint
 import re
+import sys
 from datetime import datetime
-import make_dataset
 
 import mdformat
 import requests
@@ -16,13 +14,13 @@ from mysoc_dataset import get_dataset_url
 from opml import OpmlDocument
 
 import lookup.sparql.police
+import make_dataset
 
 # TODO: Non-Home Office forces: MDP, BTP, CNC, etc
 # - Partially done - querying from CSV file works but wikidata queries need work
 # TODO: Get Disclosure Log URLs: Have Emailed MyDemocracy
 # TODO: Reduce the replication in this script.
 # - Doing this ad-hoc while I decide whether to maintain this or refactor it
-
 
 
 document: OpmlDocument = OpmlDocument(
@@ -34,7 +32,7 @@ document: OpmlDocument = OpmlDocument(
 markdown_output_file = open('output/overview.md', 'wt')
 
 
-def get_csv_dataset():
+def get_csv_dataset_from_mysociety():
     # get the url of dataset
     url = get_dataset_url(repo_name="wdtk_authorities_list",
                           package_name="whatdotheyknow_authorities_dataset",
@@ -56,8 +54,8 @@ def generate_header():
     body += "*It is generated from data provided by WhatDoTheyKnow. If there are inaccuracies, please contact* \n"
     body += "*them with corrections. The table below will then be corrected when the script is next run.* \n\n"
     body += "[OPML File Available](https://github.com/m-d-johnson/wdtk-linkgenerator/blob/master/police.opml) \n\n"
-    body += "|Organisation Name|Website|WDTK Org Page|WDTK JSON|Atom Feed|JSON Feed|Publication Scheme| \n"
-    body += "|-|-|-|-|-|-|-| "
+    body += "|Organisation Name|Website|WDTK Org Page|WDTK JSON|Atom Feed|JSON Feed|Publication Scheme|FOI Email| \n"
+    body += "|-|-|-|-|-|-|-|-| "
     return body
 
 
@@ -73,7 +71,7 @@ def process_mysociety_dataset():
         all_orgs = open("all.md", "w", encoding="utf-8")
         all_orgs.write(f"|Long Name|Short Name|Tags|")
         all_orgs.write(f"\n")
-        #all_orgs.write("|-|-|-|")
+        # all_orgs.write("|-|-|-|")
     for row in rows:
         tags_list = re.split(r"\|", row[4])
         long_name = row[1]
@@ -91,97 +89,27 @@ def process_mysociety_dataset():
     # categories,   top-level-categories,   single-top-level-category
 
 
-def process_one_csv_file(csv_file_name):
-    # Open CSV file and read it into the list of rows
-    rows = []
-    with open(csv_file_name, 'r') as csvfile:
-        csvreader = csv.reader(csvfile)
-        for row in csvreader:
-            rows.append(row)
+def make_table_from_generated_dataset():
+    json_input_file = open('data/generated-dataset.json', 'r')
+    json_dataset = json.load(json_input_file)
 
     markdown_output_file.write(generate_header() + "\n")
-
-    # Iterate over all the entries in the CSV import, and generate a row in a
-    # Markdown table
     results = []
-    for row in rows:
-        name_of_org = row[0]
-        wdtk = row[1]
-        url = f"https://www.whatdotheyknow.com/body/{wdtk}"
-        response = requests.get(f"{url}.json")
-        wdtk_data = response.json()
-
-        # If they've provided a link to a publication scheme, use that. Else,
-        # leave it set to the WDTK overview.
-        if wdtk_data['publication_scheme']:
-            pubscheme = wdtk_data['publication_scheme']
-        else:
-            pubscheme = url  # Just redirect to overview page
-
-        # If they've provided a home page, use that. Else, leave it set to the
-        # WDTK overview.
-        if wdtk_data['home_page']:
-            url = wdtk_data['home_page']
-
-        # Construct the row in the Markdown table
-        entry = ''
-        entry += f"|{name_of_org} | "
-        entry += f"[Website]({url})|"
-        entry += f"[wdtk page](https://www.whatdotheyknow.com/body/{wdtk})|"
-        entry += f"[wdtk json](https://www.whatdotheyknow.com/body/{wdtk}.json)|"
-        entry += f"[atom feed](https://www.whatdotheyknow.com/feed/body/{wdtk})|"
-        entry += f"[json feed](https://www.whatdotheyknow.com/feed/body/{wdtk}.json)|"
-        entry += f"[Publication Scheme]({pubscheme})|"
-        entry += "\n"
-
-        # results[] is a list of these rows. Append to it.
-        results.append(entry)
-
-        # Also add it to the list of RSS feeds
-        document.add_rss(f"{name_of_org} FOI Disclosures",
-                         f"https://www.whatdotheyknow.com/feed/body/{wdtk}",
-                         version='RSS2',
-                         created=datetime.now())
-
-    # For ease of reading, sort the rows alphabetically before
-    # we proceed to writing them to a file.
-    results.sort()
-    # Then, write the rows to the file and close it. The header has already been
-    # written.
-    for i in results:
-        markdown_output_file.write(str(i))
-
-
-def process_one_json_file(json_file_name):
-    fh = open(json_file_name, 'r')
-    imported_json = json.load(fh)
-    markdown_output_file.write(generate_header() + "\n")
-
-    results = []
-    for item in imported_json:
-        name_of_org = str(item['itemLabel'])
-        url = str(item['website'])
-        wdtk = str(item['wdtk'])
-        response = requests.get(f'https://www.whatdotheyknow.com/body/{wdtk}.json')
-        wdtk_data = response.json()
-        if wdtk_data['publication_scheme']:
-            pubscheme = wdtk_data['publication_scheme']
-        else:
-            pubscheme = f"https://whatdotheyknow.com/body/{wdtk}"
-
-        entry = f"|{name_of_org} | "
-        entry += f"[Website]({url})|"
-        entry += f"[wdtk page](https://www.whatdotheyknow.com/body/{wdtk})|"
-        entry += f"[wdtk json](https://www.whatdotheyknow.com/body/{wdtk}.json)|"
-        entry += f"[wdtk atom feed](https://www.whatdotheyknow.com/feed/body/{wdtk})|"
-        entry += f"[wdtk json feed](https://www.whatdotheyknow.com/feed/body/{wdtk}.json)|"
-        entry += f"[Publication Scheme]({pubscheme})|"
+    for force in json_dataset:
+        entry = f"|{force['Name']} | "
+        entry += f"[Website]({force['Home_Page_URL']})|"
+        entry += f"[wdtk page]({force['WDTK_Org_Page_URL']})|"
+        entry += f"[wdtk json]({force['WDTK_Org_JSON_URL']})|"
+        entry += f"[wdtk atom feed]({force['WDTK_Atom_Feed_URL']})|"
+        entry += f"[wdtk json feed]({force['WDTK_JSON_Feed_URL']})|"
+        entry += f"[Publication Scheme]({force['Publication_Scheme_URL']})|"
+        entry += f"[FOI Email](mailto:{force['FOI_Email_Address']})|"
         entry += "\n"
 
         results.append(entry)
 
-        document.add_rss(f"{name_of_org} FOI Disclosures",
-                         f"https://www.whatdotheyknow.com/feed/body/{wdtk}",
+        document.add_rss(f"{force['Name']} FOI Disclosures",
+                         f"{force['WDTK_Atom_Feed_URL']}",
                          version='RSS2',
                          created=datetime.now())
 
@@ -202,23 +130,23 @@ def cleanup(retain):
     else:
         print("The WDTK CSV file does not exist, so could not be deleted.")
 
-    if os.path.exists('out.md'):
-        os.unlink('out.md')
+    if os.path.exists('output/out.md'):
+        os.unlink('output/out.md')
     os.link('output/overview.md', 'output/out.md')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate overview of police forces.')
-    parser.add_argument('--mode',
-                        dest='mode',
-                        choices=['csv', 'json'],
-                        default='csv',
-                        action='store',
-                        help='Specify the source data format.')
-    parser.add_argument('-j',
-                        dest='jsonfile',
-                        action='store',
-                        help='Name of the JSON input file.')
+    parser.add_argument('--generate',
+                        dest='automatic',
+                        default='True',
+                        action='store_true',
+                        help='Generate a dataset from the emails file, then build table.')
+    parser.add_argument('--regenerate',
+                        dest='regenerate',
+                        default='False',
+                        action='store_true',
+                        help='Rebuilds a dataset from the emails file, then build table.')
     parser.add_argument('-r', '--retain',
                         action='store_true',
                         dest='retain', default=False,
@@ -227,48 +155,18 @@ if __name__ == '__main__':
                         action='store_true',
                         dest='wikidata', default=False,
                         help='Get a listing of local forces from wikidata.')
-    parser.add_argument('-x', '--devmode',
-                        action='store_true',
-                        dest='devmode', default=False,
-                        help='Run code in development.')
     args = parser.parse_args()
 
-    if args.mode == 'json' and args.jsonfile != '':
-        print("JSON mode selected")
-        process_one_json_file(args.jsonfile)
-    elif args.mode == 'csv':
-        print("CSV mode selected")
-        # NB. While it looks like this script should download a file from WDTK
-        # and use that as input, it doesn't yet do this. That's why there's a
-        # hardcoded file path below and a file checked into the git repository.
-        get_csv_dataset()
-        process_one_csv_file('data/wdtk-police.csv')
-    if args.wikidata:
-        lookup.sparql.police.get_local_police_forces_wikidata()
-
-    if args.devmode:
-        print("Dev mode")
-        get_csv_dataset()
-        process_mysociety_dataset()
+    if args.regenerate:
+        make_dataset.rebuild_dataset()
+        make_table_from_generated_dataset()
         markdown_output_file.close()
         cleanup(args.retain)
-
-    # markdown_output_file.close()
-    # cleanup(args.retain)
-
-
-
-
-
-
-
-from bs4 import BeautifulSoup
-import requests
-url = "https://en.wikipedia.org/wiki/Algorithm"
-req = requests.get(url)
-soup = BeautifulSoup(req.text, "html.parser")
-print("The href links are :")
-for link in soup.find_all('a'):
-   print(link.get('href'))
-
-   
+        sys.exit()
+    if args.automatic:
+        make_table_from_generated_dataset()
+        markdown_output_file.close()
+        cleanup(args.retain)
+        sys.exit()
+    if args.wikidata:
+        lookup.sparql.police.get_local_police_forces_wikidata()
--- make_dataset.py
+++ make_dataset.py
@@ -1,51 +1,16 @@
 import json
-import requests
-from bs4 import BeautifulSoup
-
-emails = json.load(open("data/foi-emails.json", "r"))
+from policeorganisation import PoliceOrganisation
 
 
-class PoliceOrganisation:
-    """Creates a new WDTK body from a WDTK ID and a dict containing FOI emails.
+def rebuild_dataset():
+    emails = json.load(open("data/foi-emails.json", "r"))
 
-    The FOI emails dict has keys with the value of the WDTK ID and values of
-    """
-    def __init__(self, wdtk_id, emails):
-        """Creates a WDTK Body from a WDTK ID."""
-        self.WDTK_Org_JSON_URL = f"https://www.whatdotheyknow.com/body/{wdtk_id}.json"
-        wdtk_data = requests.get(self.WDTK_Org_JSON_URL).json()
-        self.Name = wdtk_data["name"]
-        self.WDTK_ID = wdtk_id
-        self.Home_Page_URL = wdtk_data["home_page"]
-        self.WDTK_Org_Page_URL = f"https://www.whatdotheyknow.com/body/{wdtk_id}"
-        self.WDTK_Atom_Feed_URL = f"https://www.whatdotheyknow.com/feed/body/{wdtk_id}"
-        self.WDTK_JSON_Feed_URL = (
-            f"https://www.whatdotheyknow.com/feed/body/{wdtk_id}.json"
-        )
-        self.Publication_Scheme_URL = wdtk_data["publication_scheme"]
-        self.FOI_Email_Address = emails[wdtk_id]
+    list_of_forces = []
 
-        for tag in wdtk_data["tags"]:
-            if tag[0] == "dpr":
-                self.Data_Protection_Registration_Identifier = tag[1]
-            if tag[0] == "wikidata":
-                self.WikiData_Identifier = tag[1]
-            if tag[0] == "lcnaf":
-                self.LoC_Authority_ID = tag[1]
+    for entry in emails:
+        force = PoliceOrganisation(entry, emails)
+        list_of_forces.append(force.__dict__)
 
-        soupreq = requests.get(self.WDTK_Org_Page_URL)
-        soup = BeautifulSoup(soupreq.text, "html.parser")
-        for link in soup.find_all("a"):
-            if link.getText() == "Disclosure log":
-                self.Disclosure_Log_URL = link.get("href")
-
-
-list_of_forces = []
-
-for entry in emails:
-    force = PoliceOrganisation(entry, emails)
-    list_of_forces.append(force.__dict__)
-
-outfile = open('data/generated-dataset.json', 'w')
-outfile.write(json.dumps(list_of_forces, indent=4))
-outfile.close()
+    outfile = open('data/generated-dataset.json', 'w')
+    outfile.write(json.dumps(list_of_forces, indent=4))
+    outfile.close()
